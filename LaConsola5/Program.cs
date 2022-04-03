bb47e676437add7ddf4d240292d12948973fc909
JoelCAI <joelargentina12345@gmail.com>
2022-04-03 00:30:56 -0300

    Agregar archivos de proyecto.

---
+++ LaConsola5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+public class ConsoleKeyExample
+{
+    public static void Main()
+    {
+        ConsoleKeyInfo input;
+        do
+        {
+            Console.WriteLine(" Presione Control + F al mismo tiempo.");
+            Console.WriteLine("Press Esc to exit.");
+            input = Console.ReadKey(true);
+
+            StringBuilder output = new StringBuilder(
+                          String.Format("Presionó {0}", input.Key.ToString()));
+            bool modifiers = false;
+
+            if ((input.Modifiers & ConsoleModifiers.Alt) == ConsoleModifiers.Alt)
+            {
+                output.Append(" + " + ConsoleModifiers.Alt.ToString());
+                modifiers = true;
+            }
+            if ((input.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
+            {
+                if (modifiers)
+                {
+                    output.Append(" y ");
+                }
+                else
+                {
+                    output.Append(" +");
+                    modifiers = true;
+                }
+                output.Append(ConsoleModifiers.Control.ToString());
+            }
+            if ((input.Modifiers & ConsoleModifiers.Shift) == ConsoleModifiers.Shift)
+            {
+                if (modifiers)
+                {
+                    output.Append(" y ");
+                }
+                else
+                {
+                    output.Append(" + ");
+                    modifiers = true;
+                }
+                output.Append(ConsoleModifiers.Shift.ToString());
+            }
+            output.Append(".");
+            Console.WriteLine(output.ToString());
+            Console.WriteLine();
+        } while (input.Key != ConsoleKey.Escape);
+    }
+}
